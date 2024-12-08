e333f9f2fa01771a02582d82b768a52a6d1905f7
Schell Carl Scivally <efsubenovex@gmail.com>
2024-12-09 07:34:11 +1300

    fix: standardize linkage paths with forward slashes

--- crates/spirv-builder-cli/src/lib.rs
+++ crates/spirv-builder-cli/src/lib.rs
@@ -3,7 +3,7 @@
 /// Shader source and entry point that can be used to create shader linkage.
 #[derive(serde::Serialize, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Linkage {
-    pub source_path: std::path::PathBuf,
+    pub source_path: String,
     pub entry_point: String,
     pub wgsl_entry_point: String,
 }
@@ -11,7 +11,13 @@ pub struct Linkage {
 impl Linkage {
     pub fn new(entry_point: impl AsRef<str>, source_path: impl AsRef<std::path::Path>) -> Self {
         Self {
-            source_path: source_path.as_ref().to_path_buf(),
+            // Force a forward slash convention here so it works on all OSs
+            source_path: source_path
+                .as_ref()
+                .components()
+                .map(|c| c.as_os_str().to_string_lossy())
+                .collect::<Vec<_>>()
+                .join("/"),
             wgsl_entry_point: entry_point.as_ref().replace("::", ""),
             entry_point: entry_point.as_ref().to_string(),
         }
